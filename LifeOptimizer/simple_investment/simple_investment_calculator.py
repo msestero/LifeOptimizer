dde7461723727661ffb476ca9732c8d3837bee47
msestero <63122175+msestero@users.noreply.github.com>
2024-02-12 20:29:09 -0800

    I think I have a basic beginner backend

--- LifeOptimizer/simple_investment/simple_investment_calculator.py
+++ LifeOptimizer/simple_investment/simple_investment_calculator.py
@@ -1,55 +1,33 @@
 import argparse
 import sys
 import matplotlib.pyplot as plt
-
-
-def show_investment(monthly_data):
-    x = range(len(monthly_data))
-    y = monthly_data
-    plt.plot(x, y)
-
-    plt.xlabel('months')
-    plt.ylabel('dollars')
-    plt.title('Investment')
-
-    plt.show()
+import json
 
 
 NUM_MONTHS = 12
 
-def get_arguments():
-    parser = argparse.ArgumentParser()
-
-    if not sys.stdin.isatty():
-        parser.add_argument('initial_invesment', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
-    else:
-        parser.add_argument("initial_invesment", help="How much is currently invested", type=float)
-
-    parser.add_argument("--monthly", help="How much that will be invested on a monthly basis", type=float, default=0)
-    parser.add_argument("--yearly", help="How much that will be invested on a yearly basis", type=float, default=0)
-    parser.add_argument("--growth", help="The average expected growth each year", type=float, default=0.08)
-    parser.add_argument("years_of_growth", help="The number of years_of_growth you will let the money grow", type=int)
-
-    return parser.parse_args()
-
 def calculate(args):
-    if not sys.stdin.isatty():
-        result = float(args.initial_invesment.read().splitlines()[0])
-    else:
-        result = float(args.initial_invesment)
+    result = float(args["initial_investment"])
     invested = result
-    monthly_data = [invested]
-    for i in range(args.years_of_growth):
+    yearly_total = [invested]
+    monthly_total = [invested]
+    monthly_roi = []
+    for i in range(int(args["years_of_growth"])):
         for i in range(NUM_MONTHS):
-            result *= (1 + args.growth) ** (1 / NUM_MONTHS)
-            result += args.monthly
-        result += args.yearly
-        monthly_data.append(result)
-    monthly_data = list(map(lambda x: round(x, 2), monthly_data))
-    return result
+            monthly_roi.append(round(result * ((1 + float(args["yearly_growth"])) ** (1 / NUM_MONTHS) - 1), 2))
+            result *= (1 + float(args["yearly_growth"])) ** (1 / NUM_MONTHS)
+            result += float(args["monthly_contributions"])
+            result = round(result, 2)
+            monthly_total.append(result)
+        result += float(args["yearly_contributions"])
+        yearly_total.append(result)
+    monthly_total = list(map(lambda x: round(x, 2), monthly_total))
+    res = {
+        "result": result,
+        "yearly_total": yearly_total,
+        "monthly_total": monthly_total,
+        "monthly_roi": monthly_roi
+    }
+    res = json.dumps(res)
+    return res
 
-
-if __name__ == "__main__":
-    args = get_arguments()
-    result = calculate(args)
-    print(result)
--- LifeOptimizer/simple_investment/views.py
+++ LifeOptimizer/simple_investment/views.py
@@ -1,10 +1,21 @@
 from django.http import HttpResponse
 import subprocess
-import simple_investment_calculator
+from .simple_investment_calculator import calculate
 
 
 def index(request):
-    starting_value = request.GET.get('starting', '')
-    args = 
-    result = simple_investment_calculator.calculate(args)
-    return HttpResponse(f"{result}")
+    initial_investment = request.GET.get('initial_investment', '')
+    years_of_growth = request.GET.get('years_of_growth', '')
+    yearly_growth = request.GET.get('yearly_growth', 0.06)
+    monthly_contributions = request.GET.get('montly_contributions', 0)
+    yearly_contributions = request.GET.get('yearly_contributions', 0)
+
+    args = {
+        "initial_investment": initial_investment,
+        "years_of_growth": years_of_growth,
+        "yearly_growth": yearly_growth,
+        "monthly_contributions": monthly_contributions,
+        "yearly_contributions": yearly_contributions,
+    }
+    result = calculate(args)
+    return HttpResponse(result)
