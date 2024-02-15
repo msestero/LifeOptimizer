e9245854229266a8da5a5aff41ef601a47326c94
msestero <63122175+msestero@users.noreply.github.com>
2024-02-14 20:58:45 -0800

    nuked

--- LifeOptimizer/LifeOptimizer/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path("simple_investment/", include("simple_investment.urls")),
-    path("admin/", admin.site.urls),
-]
--- LifeOptimizer/simple_investment/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SimpleInvestmentConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'simple_investment'
