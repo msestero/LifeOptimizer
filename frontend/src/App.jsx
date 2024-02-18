a61f39c5df8380788fe9d438e4a1cde69e348cdf
msestero <63122175+msestero@users.noreply.github.com>
2024-02-18 14:22:35 -0800

    foundation of basic investment calc done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import InvestmentForm from "./InvestmentForm";
+import {Chart as ChartJS} from "chart.js/auto";
+
+function App() {
+
+  return (
+    <div className="App">
+      <InvestmentForm />
+    </div>
+  )
+}
+
+export default App
